fabc114684a3d602518bf7e25ca39589fbc62575
PeterPirog <56269734+PeterPirog@users.noreply.github.com>
2026-09-09 23:24:52 +0200

    Export paired Red ablation API

--- src/llm_redteam/red/__init__.py
+++ src/llm_redteam/red/__init__.py
@@ -1,5 +1,14 @@
 """Adaptive Red Team planning primitives."""
 
+from .ablation import (
+    AblationArm,
+    PairedRedAblationContract,
+    PairedRedAblationReport,
+    PairedRedObservation,
+    PairingMode,
+    observation_from_run,
+    summarize_paired_red_ablation,
+)
 from .adaptive import (
     AdaptiveRedStrategy,
     RedAction,
@@ -24,6 +33,7 @@ from .mechanisms import (
 from .sequence_metrics import RedSequenceMetrics, SequenceRate, summarize_red_sequences
 
 __all__ = [
+    "AblationArm",
     "AdaptiveRedEfficiency",
     "AdaptiveRedStrategy",
     "AttackHypothesis",
@@ -37,6 +47,10 @@ __all__ = [
     "MechanismMemorySnapshot",
     "MechanismPolicy",
     "MechanismRedDecision",
+    "PairedRedAblationContract",
+    "PairedRedAblationReport",
+    "PairedRedObservation",
+    "PairingMode",
     "RedAction",
     "RedCampaignMemory",
     "RedDecision",
@@ -47,6 +61,8 @@ __all__ = [
     "RedPlanner",
     "RedSequenceMetrics",
     "SequenceRate",
+    "observation_from_run",
     "summarize_adaptive_red",
+    "summarize_paired_red_ablation",
     "summarize_red_sequences",
 ]
