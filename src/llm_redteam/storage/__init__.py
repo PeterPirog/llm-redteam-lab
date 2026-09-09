11a1a2442ff703b16348043b557c33e75d55102c
PeterPirog <56269734+PeterPirog@users.noreply.github.com>
2026-09-09 23:39:20 +0200

    Export paired ablation provenance API

--- src/llm_redteam/storage/__init__.py
+++ src/llm_redteam/storage/__init__.py
@@ -1,5 +1,16 @@
 """Persistence layer for experiment evidence, genealogy and measurement provenance."""
 
+from .ablation_models import RedAblationExperimentRow, RedAblationObservationRow
+from .ablation_repository import (
+    RedAblationExperimentSnapshot,
+    build_red_ablation_experiment_snapshot,
+    load_red_ablation_experiment,
+    load_red_ablation_observations,
+    save_red_ablation_experiment,
+    save_red_ablation_observation,
+    save_red_ablation_observations,
+    summarize_persisted_red_ablation,
+)
 from .evaluation_set_models import EvaluationSetManifestRow
 from .evaluation_set_repository import (
     load_evaluation_set_manifest,
@@ -12,8 +23,10 @@ from .measurement_repository import (
     build_campaign_measurement_snapshot,
     build_evaluation_campaign_measurement_snapshot,
     fingerprint_attack_policy,
+    fingerprint_budget,
     fingerprint_case_set,
     fingerprint_corpus_snapshot,
+    fingerprint_judge_policy,
     load_campaign_measurement_snapshot,
     save_campaign_measurement_snapshot,
 )
@@ -26,14 +39,26 @@ __all__ = [
     "CampaignMeasurementSnapshot",
     "EvaluationSetManifestRow",
     "ExperimentRepository",
+    "RedAblationExperimentRow",
+    "RedAblationExperimentSnapshot",
+    "RedAblationObservationRow",
     "build_campaign_measurement_snapshot",
     "build_evaluation_campaign_measurement_snapshot",
+    "build_red_ablation_experiment_snapshot",
     "fingerprint_attack_policy",
+    "fingerprint_budget",
     "fingerprint_case_set",
     "fingerprint_corpus_snapshot",
+    "fingerprint_judge_policy",
     "load_campaign_measurement_snapshot",
     "load_evaluation_set_manifest",
     "load_evaluation_set_manifest_by_id",
+    "load_red_ablation_experiment",
+    "load_red_ablation_observations",
     "save_campaign_measurement_snapshot",
     "save_evaluation_set_manifest",
+    "save_red_ablation_experiment",
+    "save_red_ablation_observation",
+    "save_red_ablation_observations",
+    "summarize_persisted_red_ablation",
 ]
